67553d66a88e31d90c63d5165b02564cfcd05ba5
rubiwachs <rubiwachs@gmail.com>
2015-02-08 14:20:34 +0100

    change hello text

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ var React = require("react");
 
 var App = React.createClass({
     render() {
-        return <h1>Hello changedsedd</h1>;
+        return <h1>Hello chang das ass dd dedsedd</h1>;
     }
 });
 
